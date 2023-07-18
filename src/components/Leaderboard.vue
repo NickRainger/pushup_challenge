538687b92ae8a56db806f6e9785e093a1a358edb
nickrainger <rainger2006@gmail.com>
2023-07-18 08:24:16 +0200

    123

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -6,15 +6,14 @@
     <div v-for="groupUser, i in  store.groupUsers " :key="groupUser.id" class="text-xl font-bold flex items-center gap-2">
 
 
-      <div v-if="i == 0 && canShow(<ExtendedGroupUser>groupUser)" class="text-3xl material-symbols-rounded fill"
-        :class="{ 'text-amber-400': (groupUser.totalReps || 0) >= 100 }">
-        star
-      </div>
+      <div v-if="i == 0 && (groupUser.totalReps || 0) >= 100"
+        class="text-3xl material-symbols-rounded fill text-amber-400">star</div>
 
-      <div v-else-if="i == 1 && canShow(<ExtendedGroupUser>groupUser)"
-        class=" text-3xl text-[#898d90] material-symbols-rounded fill">star</div>
-      <div v-else-if="i == 2 && canShow(<ExtendedGroupUser>groupUser)"
-        class=" text-3xl text-[#b06c28] material-symbols-rounded fill">star</div>
+      <div v-else-if="i == 1 && (groupUser.totalReps || 0) >= 100"
+        class="text-3xl text-[#898d90]  material-symbols-rounded fill">star</div>
+      <div v-else-if="i == 2 && (groupUser.totalReps || 0) >= 100"
+        class=" text-3xl text-[#b06c28] material-symbols-rounded fill" :class="{ '': (groupUser.totalReps || 0) >= 100 }">
+        star</div>
 
       <div v-else-if="(groupUser.totalReps || 0) >= 100" class="text-3xl material-symbols-rounded fill text-emerald-500">
         done_all
@@ -40,10 +39,10 @@ import { formatTime } from '@/utils';
 import store from "@/store"
 import type { ExtendedGroupUser } from '@/types';
 
-function canShow(groupUser: ExtendedGroupUser): boolean {
-  return (!store.groupUsers.some(user => user.id != groupUser.id && groupUser.totalReps == user.totalReps) ||
-    (groupUser.totalReps || 0) >= 100) &&
-    groupUser.totalReps != 0
-}
+// function canShow(groupUser: ExtendedGroupUser): boolean {
+//   return (!store.groupUsers.some(user => user.id != groupUser.id && groupUser.totalReps == user.totalReps) ||
+//     (groupUser.totalReps || 0) >= 100) &&
+//     groupUser.totalReps != 0
+// }
 
 </script>
